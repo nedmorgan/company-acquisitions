c1511dfc1429c872ae2ec1efd281d27b4138f3fc
nedmorgan <nmorgan1987@gmail.com>
2019-04-09 16:02:04 -0400

    preparing for heroku deployment

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(
 
 app.use(express.json());
 
+app.use(express.static(`${__dirname}/client/build`));
+
 app.get("/*", (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`);
 });

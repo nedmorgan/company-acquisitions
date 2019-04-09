5dde734efdcfba0d098da30af4b5e69b3f36e754
nedmorgan <nmorgan1987@gmail.com>
2019-04-09 16:16:36 -0400

    created schema for company and contacts

---
+++ db/schema.js
@@ -0,0 +1,20 @@
+const mongoose = require("./connection");
+const Schema = mongoose.Schema;
+
+const ContactSchema = new Schema({
+  name: String,
+  phoneNumber: String
+});
+
+const CompanySchema = new Schema({
+  title: String,
+  status: String,
+  information: String,
+  financialPerformance: String,
+  contacts: [ContactSchema]
+});
+
+module.exports = {
+  ContactSchema: ContactSchema,
+  CompanySchema: CompanySchema
+};

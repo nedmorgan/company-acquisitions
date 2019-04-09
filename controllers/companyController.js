1311af5c9cc3214e382e1fa334bd5708d6583398
nedmorgan <nmorgan1987@gmail.com>
2019-04-09 19:02:29 -0400

    routes working to show company index and specific company

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -3,13 +3,21 @@ const Contact = require('../models/Contact')
 
 const companyController = {
     index: (req, res) => {
-        res.send('Index page for company')
+        Company.find().then((company) => {
+            res.json(company)
+        }).catch((err) => {
+            console.log('Error finding company: ', err)
+        })
     },
     create: (req, res) => {
         res.send('Action to create a new company')
     },
     show: (req, res) => {
-        res.send("Page to show a company")
+        Company.findById(req.params.companyId).then((company) => {
+            res.json(company)
+        }).catch((err) => {
+            console.log('Error finding specific company: ', err)
+        })
     },
     update: (req, res) => {
         res.send('Action to update a company')

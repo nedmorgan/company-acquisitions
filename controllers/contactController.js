942261672fedcf8756499c81cd7b835d63006978
nedmorgan <nmorgan1987@gmail.com>
2019-04-09 19:25:35 -0400

    bug with adding a contact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -12,7 +12,17 @@ const contactController = {
             })
     },
     create: (req, res) => {
-        res.send('Action to create a contact')
+        Company.findById(req.params.companyId)
+            .then(company => {
+                const newContact = new Contact(req.body)
+                company.contacts.push(newContact)
+                company.save()
+                    .then(company => {
+                        res.json(company)
+                    })
+            }).catch((err) => {
+                console.log("Error adding a new contact: ", err)
+            })
     },
     show: (req, res) => {
         Company.findById(req.params.companyId)
@@ -27,7 +37,16 @@ const contactController = {
         res.send('Action to update a contact')
     },
     delete: (req, res) => {
-        res.send('Action to delete a contact')
+        Company.findById(req.params.companyId)
+            .then(company => {
+                const specificContacts = company.contacts.filter(contact => contact._id.toString() !== req.params.contactId)
+                company.contacts = specificContacts
+                company.save().then(company => {
+                    res.json(company.contacts)
+                })
+            }).catch((err) => {
+                console.log("Error removing a company contact: ", err)
+            })
     }
 }
 

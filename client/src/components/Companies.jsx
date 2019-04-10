fa58910923366669fd43ba903939bace873dadab
nedmorgan <nmorgan1987@gmail.com>
2019-04-10 14:12:31 -0400

    able to obtain company data and set state

--- client/src/components/Companies.jsx
+++ client/src/components/Companies.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react'
-import Company from './Company'
 import { CompaniesContainer } from './styled_components/CompaniesStyles'
+import { Redirect } from 'react-router-dom'
+import Company from './Company'
+import axios from 'axios'
 
 export default class Companies extends Component {
+
+    state = {
+        company: []
+    }
+
+    componentDidMount() {
+        this.getCompanyData()
+    }
+
+    getCompanyData = () => {
+        axios.get('api/v1/companies').then(response => {
+            console.log(response.data)
+            this.setState({ company: response.data })
+        })
+    }
+
     render() {
         return (
             <CompaniesContainer>
